623770ab09fb4752d97a96d506e0c7d2625ecc4a
Rajnish Singh <79822364+coderboy1952@users.noreply.github.com>
2021-11-03 20:23:00 +0530

    Add files via upload

---
+++ data/scripts/getPermissionsHashes.py
@@ -0,0 +1,5 @@
+ownerHashes = ['\xee\x80\xa0Stormbreaker3095','\xee\x80\xb0PC355538','\xee\x80\xb0Android ID']
+adminHashes = ['\xee\x80\xa0Stormbreaker3095','\xee\x80\xb0PC355538','\xee\x80\xb0Android ID']
+vipHashes = ['\xee\x80\xa0','\xee\x80\xb0PC ID','\xee\x80\xb0Android ID']
+
+
